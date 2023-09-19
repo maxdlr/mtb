8a242f25df551e8a6997fe3b927205787c745dde
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-09-19 14:07:03 +0200

    Add: post entity, controller, type, templates.

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
