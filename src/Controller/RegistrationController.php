a7e4161ebbe61b8336ce3155a976461eda70357a
maxdlr <contact@maxdlr.com>
2023-11-16 14:46:50 +0100

    feat: improved regsitration flashes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Page;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use App\Service\SecurityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,7 +22,8 @@ class RegistrationController extends AbstractController
         Request                     $request,
         UserPasswordHasherInterface $userPasswordHasher,
         EntityManagerInterface      $entityManager,
-        Security                    $security
+        Security                    $security,
+        SecurityManager             $securityManager
     ): Response
     {
         if ($this->getUser()) {
@@ -36,23 +38,31 @@ class RegistrationController extends AbstractController
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            )
-                ->setPage($page)
-                ->setRegistrationDate($now);
+        if ($form->isSubmitted()) {
 
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('success', 'Compte créé.');
-            // do anything else you need here, like send an email
-            $security->login($user, 'form_login');
-            return $this->redirectToRoute('app_redirect_user_fallback');
+            if ($securityManager->doesUsernameExist($form->getData()->getUsername())) {
+                $this->addFlash('danger', 'Le nom d\'utilisateur "' . ucfirst($user->getUsername()) . '" existe déjà !');
+                return $this->redirectToRoute('app_redirect_referer');
+            };
+
+            if ($form->isValid()) {
+
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                )
+                    ->setPage($page)
+                    ->setRegistrationDate($now);
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('success', 'Compte créé. Bienvenue ' . ucfirst($user->getUsername()) . ". Voila votre page !");
+                // do anything else you need here, like send an email
+                $security->login($user, 'form_login');
+                return $this->redirectToRoute('app_redirect_user_fallback');
+            }
         }
 
         return $this->render('auth/register-index.html.twig', [
